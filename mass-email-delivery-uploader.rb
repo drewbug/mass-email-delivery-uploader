ec85f7948bfe38d23e2694b72c98a69fe6d6cd5e
Drew Carey Buglione <drewcbuglione@gmail.com>
2013-06-18 00:29:30 +0100

    Status page!

--- mass-email-delivery-uploader.rb
+++ mass-email-delivery-uploader.rb
@@ -24,3 +24,19 @@ post '/upload' do
   
   "Success! Files created from #{filename}: #{out_files}"
 end
+
+get '/status' do
+  status = {}
+
+  Dir.glob('*.csv.stat').each do |fname|
+    max = `wc -l #{fname.chomp('.stat')}`.split.first.to_i+1
+    complete = `wc -l #{fname}`.split.first.to_i+1
+
+    matches = fname.match(/^(.*)_(\d+).csv.stat$/)
+
+    status[matches[1]] ||= {}
+    status[matches[1]][matches[2]] = [ max, complete ]
+  end
+
+  erb :status, :locals => {:status => status}
+end
