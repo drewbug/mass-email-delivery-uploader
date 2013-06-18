74f3cfd5cf328c3f4dc29a2e7b27cb2879592371
Paul Nickerson <paul.nickerson@grooveshark.com>
2013-06-17 23:23:43 -0400

    everything seems to be working

--- mass-email-delivery-uploader.rb
+++ mass-email-delivery-uploader.rb
@@ -1,44 +1,42 @@
 require 'rubygems'
 require 'sinatra'
 
+secret_path="stopwatchingus_uploader_123"
+
 configure do
   $directory = ARGV.any? ? ARGV[0] : '/tmp'
 end
 
-get '/' do
+get '/' + secret_path + '/' do
   erb :form
 end
 
-post '/upload' do
+post '/' + secret_path + '/upload' do
   filename = params[:file][:filename]
   tempfile = params[:file][:tempfile]
 
-  out_files = IO.foreach(tempfile).each_slice(params[:slice_size].to_i).each_with_index.map do |slice, index|
+  out_files = tempfile.each.each_slice(params[:slice_size].to_i).each_with_index.map do |slice, index|
     out_file = "#{File.basename(filename, '.*')}_#{index}.csv"
     Dir.chdir($directory) { File.open(out_file, 'w') { |f| f.puts(slice) } }
     out_file
   end
 
-  Dir.chdir($directory) { `screen -d -m -S #{filename}` }
+  curr_path = File.expand_path(File.dirname(__FILE__))
 
   out_files.each do |out_file|
-    python_command = "python mymain.py #{out_file} #{out_file}.stat > #{out_file}.stdout 2> #{out_file}.stderr &"
-    `screen -S #{filename} -X #{python_command}`
+    `python #{curr_path}/script_caller.py \"#{$directory}/#{out_file}\" \"#{$directory}/#{out_file}.stat\" > #{out_file}.stdout 2> #{out_file}.stderr &`
   end
   
-  "Success! Files created from #{filename}: #{out_files}"
+  "Success! Files created from #{filename}: #{out_files}<br><a href='.'>Click here to return to the main screen</a>"
 end
 
-get '/status' do
+get '/' + secret_path + '/status' do
   status = {}
+  Dir.glob($directory + '/*.csv.stat').each do |fname|
+    max = `wc -l "#{fname.chomp('.stat')}"`.split.first.to_i+1
+    complete = `wc -l "#{fname}"`.split.first.to_i+1
 
-  Dir.glob('*.csv.stat').each do |fname|
-    Dir.chdir($directory) do
-      max = `wc -l #{fname.chomp('.stat')}`.split.first.to_i+1
-      complete = `wc -l #{fname}`.split.first.to_i+1
-    end
-
-    matches = fname.match(/^(.*)_(\d+).csv.stat$/)
+    matches = fname.match(/(.*)_(\d+).csv.stat$/)
 
     status[matches[1]] ||= {}
     status[matches[1]][matches[2]] = [ max, complete ]
--- script_caller.py
+++ script_caller.py
@@ -4,7 +4,6 @@ import os
 
 #hardcoded settings
 path_to_mass_emailer = "/Users/paul/mass-email-delivery-code/"
-path_to_working_files = "/tmp/"
 path_to_message_file = path_to_mass_emailer + "noCispaMessage.txt"
 dry_run = True
 
@@ -16,8 +15,11 @@ senate_emailer = imp.load_source("EmailSenateFromCSV-FFTF", path_to_mass_emailer
 house_emailer = imp.load_source("EmailHouseFromCSV-FFTF", path_to_mass_emailer + "EmailHouseFromCSV-FFTF.py")
 
 if __name__ == "__main__":
-    csvfile = path_to_working_files + sys.argv[1]
-    statfile = path_to_working_files + sys.argv[2]
+    csvfile = sys.argv[1]
+    statfile = sys.argv[2]
     #do these sequentially because this file should already have been launched in parallel with other instances
     senate_emailer.csv_Send_To_Senate(csvfile, path_to_message_file, statfile, dryrun = dry_run)
     house_emailer.csv_Send_To_House(csvfile, path_to_message_file, statfile, dryrun = dry_run)
+
+    os.remove(csvfile)
+    os.remove(statfile)
