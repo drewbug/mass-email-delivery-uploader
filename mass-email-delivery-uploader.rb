35d555e5727f312e894e135ebef6faa4e30ce4f0
Drew Carey Buglione <drewcbuglione@gmail.com>
2013-06-18 02:20:42 +0100

    Rudimentary directory changing

--- mass-email-delivery-uploader.rb
+++ mass-email-delivery-uploader.rb
@@ -1,6 +1,10 @@
 require 'rubygems'
 require 'sinatra'
 
+configure do
+  $directory = ARGV.any? ? ARGV[0] : '/tmp'
+end
+
 get '/' do
   erb :form
 end
@@ -11,11 +15,12 @@ post '/upload' do
 
   out_files = IO.foreach(tempfile).each_slice(params[:slice_size].to_i).each_with_index.map do |slice, index|
     out_file = "#{File.basename(filename, '.*')}_#{index}.csv"
-    File.open(out_file, 'w') { |f| f.puts(slice) }
+    Dir.chdir($directory) { File.open(out_file, 'w') { |f| f.puts(slice) } }
     out_file
   end
 
-  `screen -d -m -S #{filename}`
+  Dir.chdir($directory) { `screen -d -m -S #{filename}` }
+
   out_files.each do |out_file|
     python_command = "python mymain.py #{out_file} #{out_file}.stat > #{out_file}.stdout 2> #{out_file}.stderr &"
     `screen -S #{filename} -X #{python_command}`
@@ -28,8 +33,10 @@ get '/status' do
   status = {}
 
   Dir.glob('*.csv.stat').each do |fname|
-    max = `wc -l #{fname.chomp('.stat')}`.split.first.to_i+1
-    complete = `wc -l #{fname}`.split.first.to_i+1
+    Dir.chdir($directory) do
+      max = `wc -l #{fname.chomp('.stat')}`.split.first.to_i+1
+      complete = `wc -l #{fname}`.split.first.to_i+1
+    end
 
     matches = fname.match(/^(.*)_(\d+).csv.stat$/)
 
